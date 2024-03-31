9abcf3f88b18a29774b6ccf5f75d78b8c24a8890
Andy Smith <andysmith836@gmail.com>
2024-03-31 23:06:42 +0000

    Add scripts to install the controller node Make using microk8s easier

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ansible-playbook -i inventory/pis.yaml playbook.yaml -K
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible iptables-persistent kea -y
+
+# ansible-playbook -i inventory/pis.yaml playbook.yaml -K
