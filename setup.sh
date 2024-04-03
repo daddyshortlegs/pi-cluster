b5e3aca400c8da85fe9050e037e774bfd976d08d
Andy Smith <andysmith836@gmail.com>
2024-04-03 22:00:07 +0000

    Add iptables stuff

--- setup.sh
+++ setup.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt update
-sudo apt install ansible iptables-persistent kea -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt update -y
+sudo apt install ansible -y
 sudo cp templates/etc/netplan/01-netcfg-master.yaml /etc/netplan
 sudo netplan apply
+
+# Should template port forwarding file here (sysctl)
+
+sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+sudo apt install iptables-persistent kea -y
+sudo sh -c "iptables-save > /etc/iptables/rules.v4"
+
 sudo cp templates/etc/kea/kea-dhcp4.conf /etc/kea
 sudo systemctl restart kea-dhcp4-server
+
+
+
+
+# This stuff should be separate as the worker nodes may not be ready
+ssh-keygen -t rsa -b 4096
+ssh-copy-id pi@10.0.0.2
+ssh-copy-id pi@10.0.0.3
+ssh-copy-id pi@10.0.0.4
 
 
 # ansible-playbook -i inventory/pis.yaml playbook.yaml -K
