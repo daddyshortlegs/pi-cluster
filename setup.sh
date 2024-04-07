61c32e068cd0a3ee60c7a8ddfed53eefcfdf5d64
Andy Smith <andysmith836@gmail.com>
2024-04-07 21:34:20 +0000

    Set up port forwarding

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ sudo apt install ansible -y
 sudo cp templates/etc/netplan/01-netcfg-master.yaml /etc/netplan
 sudo netplan apply
 
-# Should template port forwarding file here (sysctl)
-
+sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 sudo apt install iptables-persistent kea -y
 sudo sh -c "iptables-save > /etc/iptables/rules.v4"
@@ -15,14 +14,10 @@ sudo sh -c "iptables-save > /etc/iptables/rules.v4"
 sudo cp templates/etc/kea/kea-dhcp4.conf /etc/kea
 sudo systemctl restart kea-dhcp4-server
 
-
-
+ssh-keygen -t rsa -b 4096
 
 # This stuff should be separate as the worker nodes may not be ready
-ssh-keygen -t rsa -b 4096
-ssh-copy-id pi@10.0.0.2
-ssh-copy-id pi@10.0.0.3
-ssh-copy-id pi@10.0.0.4
+#ssh-copy-id pi@10.0.0.2
+#ssh-copy-id pi@10.0.0.3
+#ssh-copy-id pi@10.0.0.4
 
-
-# ansible-playbook -i inventory/pis.yaml playbook.yaml -K
