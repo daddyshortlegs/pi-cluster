a070ec4ff84038aa5e159e06e45c34d3ff539e4c
Andy Smith <andysmith836@gmail.com>
2024-04-11 21:02:42 +0000

    Tidy

--- k8s.sh
+++ k8s.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i inventory/pis.yaml microk8s.yaml -K --ask-pass
--- set_hostnames.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -i inventory/pis.yaml  workers.yaml -K --ask-pass
-
